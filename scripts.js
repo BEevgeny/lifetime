ad320251a926dded228c0b8a3a44f35d669d2aed
BEvgeny <o.velikij@gmail.com>
2018-03-01 16:10:05 +0300

    repaired the calculation formula

--- scripts.js
+++ scripts.js
@@ -1,30 +1,40 @@
 var day, moon, years;
 var nowDate = new Date();
+var wordDay;
+
+
+
+//Функция вывода текста
+function nameDay() {
+	console.log('Вывод текста - здни заменены');
+
+	wordDay = Number(document.getElementById('outputDay').value);
+	if (wordDay > 0) {
+		wordDay = 'Денечек';
+	}
+	document.getElementById('inputNameDay').innerHTML = "Дней";
+}
+
 
 
 
 //Функция вывода в консоль цифры кол-ва дней
-function consDay() {
+function submit() {
 	day = Number(document.getElementById('inputSelectDay').value);
-	console.log('Прошло '+day+' дней');
-}
-
-function consMoon() {
 	moon = Number(document.getElementById('inputSelectMonth').value);
-	console.log('Прошло '+moon+' месяца');
-}
-
-function consYear() {
 	years = Number(document.getElementById('inputSelectYears').value);
 	var	yourDate = new Date(years, moon-1, day);
-	var	yourSek = parseInt(nowDate-yourDate/10000);
+	var	yourSek = parseInt((nowDate-yourDate)/1000); /*  */
 	var	yourMinutes = parseInt(yourSek/60);
 	var	yourHour = parseInt(yourMinutes/60);
 	var	yourDay = parseInt(yourHour/24);
 
 	document.getElementById('outputDay').innerHTML = yourDay;
+	document.getElementById('outputHour').innerHTML = yourHour;
 	document.getElementById('outputMinutes').innerHTML = yourMinutes;
 	document.getElementById('outputSek').innerHTML = yourSek;
+
+nameDay()
 
 	console.log('Вы родились в '+years+' году');
 	console.log('Вам '+yourSek+' секунд');
